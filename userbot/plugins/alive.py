e43f1e71af2dda7f37a3d965da631c0470ac321e
Mr_Googleboy <67076946+mr-googleboy@users.noreply.github.com>
2020-06-24 06:07:10 +0530

    alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -19,7 +19,7 @@ async def amireallyalive(alive):
                      "☞Yes Sir ! I'm Alive\n"
                      f"`☞Telethon version: {version.__version__}\n`"
                      f"`☞Python: {python_version()}\n`"
-                     f"☞Bot Creater : Ankith, Sandeep\n"
+                     f"☞Bot Creater : #Mr_Googleboy\n"
                      f"`☞My peru owner`: {DEFAULTUSER}\n\n"
                      "☞Join [Channel](https://t.me/super_botz) For Latest Updates\n"
                      "[Deploy this super bot](https://github.com/Ankith09/ankithuserbot)"
